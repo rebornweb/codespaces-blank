d5728fb33b44ae6dc001a96edc5c0d815ce70000
Reborn Web <andrei.nicolas@hotmail.com>
2023-04-19 04:22:32 +0000

    Fix jQuery

--- Work_examples/JS_testing/src/scriptjQuery.js
+++ Work_examples/JS_testing/src/scriptjQuery.js
@@ -1,15 +1,14 @@
 
 //This is to prevent autofill from taking Email as Username
-  const cloneAndInsertUsernamejQuery = () => {
-    const cloneElem = document.querySelector('#newSignInName').cloneNode(true);
-  
-    // Firefox
-    cloneElem.id = 'usernamehidden';
-    // Safari
-    cloneElem.autocomplete = 'username';
-    document.querySelector('#email').before(cloneElem);
-  };
-  
+const cloneAndInsertUsernamejQuery = function () {
+  let cloneElem = $('#newSignInName').clone(true);
+
+  // Firefox
+  cloneElem.attr('id', 'usernamehidden');
+  // Safari
+  cloneElem.prop('autocomplete', 'username'); // Use prop() to set autocomplete attribute
+  $('#email').before(cloneElem);
+};
   
   module.exports = cloneAndInsertUsernamejQuery;
  
